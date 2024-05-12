a11eebb41598aa6d905945192f7b445d6d799654
dmitry_rodin <dimoon-96@yandex.ru>
2024-05-12 16:41:14 +0700

    add create table url_check

--- database.sql
+++ database.sql
@@ -2,4 +2,14 @@ CREATE TABLE urls(
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255),
     created_at DATE
-)
+);
+
+CREATE TABLE url_check(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    irl_id BIGINT REFERENCES urls(id),
+    status_code INT,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    created_at DATE
+);
