e09391dc29d6ff23b771c13894f64220d26b0389
dmitry_rodin <dimoon-96@yandex.ru>
2024-05-14 03:03:46 +0700

    add DROP TABLE

--- database.sql
+++ database.sql
@@ -1,12 +1,16 @@
-CREATE TABLE urls(
+DROP TABLE urls;
+
+DROP TABLE url_check;
+
+CREATE TABLE urls (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255),
     created_at DATE
 );
 
-CREATE TABLE url_check(
+CREATE TABLE url_check (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    irl_id BIGINT REFERENCES urls(id),
+    url_id BIGINT REFERENCES urls(id),
     status_code INT,
     h1 VARCHAR(255),
     title VARCHAR(255),
