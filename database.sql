a190f06c572bc7ab2d49fa9eb877ea15c420da58
dmitry_rodin <dimoon-96@yandex.ru>
2024-05-15 15:40:45 +0700

    update sql requests

--- database.sql
+++ database.sql
@@ -1,19 +1,19 @@
-DROP TABLE urls;
+DROP TABLE url_checks;
 
-DROP TABLE url_check;
+DROP TABLE urls;
 
 CREATE TABLE urls (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255),
-    created_at DATE
+    created_at DATE DEFAULT NOW()
 );
 
-CREATE TABLE url_check (
+CREATE TABLE url_checks (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     url_id BIGINT REFERENCES urls(id),
     status_code INT,
     h1 VARCHAR(255),
     title VARCHAR(255),
     description TEXT,
-    created_at DATE
+    created_at DATE DEFAULT NOW()
 );
